b37721bed30afec9b7643276389abd01e9f641bf
Emmanuel Apau <e@mechanicode.io>
2021-08-31 20:08:18 -0400

    clean up and using flask run

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import os
-from wsgiref import simple_server
 import socket
 import jinja2
 from flask import Flask, request
 from flask import render_template
 from pprint import pprint
 import config
-
-
-def load_template(name):
-    path = os.path.join('templates', name)
-    with open(os.path.abspath(path), 'r') as fp:
-        return jinja2.Template(fp.read())
-
-
-# class HelloResource(object):
-#     def on_get(self, req, res):
-#         hostname = socket.gethostname()
-#
-#         template = load_template('index.j2.html')
-#         res.status = falcon.HTTP_200
-#         res.content_type = 'text/html'
-#         res.body = template.render(hostname=hostname, headers=req.headers)
-
 
 app = Flask(__name__)
 
@@ -40,6 +22,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    httpd = simple_server.make_server(config.HOST, config.PORT, app)
     print(f'Ready to receive requests on {config.PORT}')
-    httpd.serve_forever()
+    app.run(host='0.0.0.0', port=config.PORT, threaded=True)
