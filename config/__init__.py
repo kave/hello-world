f404069582113cc7c8b84b24cc0c536f0ba0f257
Emmanuel Apau <e@mechanicode.io>
2021-09-02 19:52:21 -0400

    uncomplicate startup

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,4 @@
 import os
-from os.path import dirname, join
-
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path, verbose=True)
 
 # Server
 HOST = os.environ.get('HOST', '0.0.0.0')
