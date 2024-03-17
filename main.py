e2e16e7e20d569c743984880a8d1a416a4f70a43
kave <etapau@gmail.com>
2024-03-17 12:32:02 -0400

    update language so its more clear health checks

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def hello_world():
 
 @app.route("/<string:path>")
 def catch_all(path):
-    print('Failed health check you want to ping /healthz')
+    print('Failed health check! Update the image to ping path /healthz')
     return make_response({'msg': f'unknown path'}, 500)
 
 
